e3792f26e71450c8aa947f79c3ce11738cacd2d2
orhan5142 <xaxzade@gmail.com>
2026-05-16 23:21:27 +0400

    all codes

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const salam = [];
+salam.push(1);
+salam.push("hello");
+salam.push(true);
+salam.push(undefined);
+console.log(salam);
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+const salam: (number | string | boolean | undefined)[] = [];
+salam.push(1);
+salam.push("hello");
+salam.push(true);
+salam.push(undefined);
+console.log(salam);
